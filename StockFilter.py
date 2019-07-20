992e7865633a722b35514d148fdbca2cd62c3b0a
justinmulli <37046316+justinmulli@users.noreply.github.com>
2019-07-20 22:44:35 +0200

    Delete StockFilter.py

--- StockFilter.py
+++
@@ -1,8 +0,0 @@
-def StockFilter(frame, tickercol, *keep):
-	import pandas as pd
-	singles = list(dict.fromkeys(frame[tickercol]))
-	filtered = pd.DataFrame(index = frame[frame[tickercol] == singles[0]].index)
-	for ticker in singles:
-		for col in keep:
-			filtered[ticker + " " + col] = frame[frame[tickercol] == ticker][col]
-	return filtered
