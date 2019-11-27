6e63f3227eaa9c0c47278884c57b49ad23e3d361
Justus Mulli <justinmulli@gmail.com>
2019-11-27 22:49:21 +0100

    removing unused api request functions

--- apihelpers.py
+++ apihelpers.py
@@ -3,22 +3,6 @@ import time
 import requests
 from bs4 import BeautifulSoup
 
-def get_daily_stocks(ticker, key):
-    print(ticker)
-
-    temp = pd.read_csv('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=%s&outputsize=full&apikey=%s&datatype=csv'
-        % (ticker, key))
-
-    time.sleep(10)
-    temp.to_csv('Data/Day/%s.csv' % (ticker))
-
-def get_daily_adjusted_stocks(ticker, outputsize = 'full'):
-    print(ticker)
-    temp = pd.read_csv('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=%s&outputsize=%s&apikey=%s&datatype=csv'
-                     % (ticker, outputsize, key))
-    time.sleep(10)
-    temp.to_csv('Data/Adjusted/%s-Daily-Adjusted.csv' % (ticker))
-
 def get_SP500_list():
     sp = requests.get('https://www.slickcharts.com/sp500')
     sp = BeautifulSoup(sp.content, 'html.parser')
@@ -34,7 +18,6 @@ def get_SP500_list():
     return SP500
 
 def get_minute_data(ticker, key):
-
 
     try:
         frame = pd.read_csv(
